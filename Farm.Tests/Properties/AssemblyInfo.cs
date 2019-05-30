f4887df326bd7d4393d8cac3f8ace29c885cab1a
david griffiths <davidgriffo@gmail.com>
2019-05-30 15:34:24 +1000

    add some unit tests

---
+++ Farm.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Farm.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Farm.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("573c2439-eeef-4ad9-8236-1257d580bc03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Farm.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FarmSystem.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Farm.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCows()
+        {
+            //arrange
+            var cow = new Cow();
+            //act
+            //assert
+            Assert.IsTrue(cow.NoOfLegs == 4);
+            Assert.IsTrue(cow.AnimalType == "Cow");
+            Assert.IsNotNull(cow.Id, "id should not be null!");
+            Assert.IsTrue(cow is IMilkableAnimal);
+
+        }
+        [TestMethod]
+        public void TestAnimals()
+        {
+            var horse = new Horse();
+            Assert.IsFalse(horse is IMilkableAnimal);
+
+            var chook = new Hen();
+            Assert.IsTrue(chook.NoOfLegs == 2);
+            Assert.IsFalse(chook is IMilkableAnimal);
+
+        }
+    }
+}
