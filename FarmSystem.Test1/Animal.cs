eef098f32cf8882068d9955ac1d529e53deb4047
david griffiths <davidgriffo@gmail.com>
2019-05-30 15:29:08 +1000

    new abstract class for animal

---
+++ FarmSystem.Test1/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FarmSystem.Test
+{
+
+    public abstract class Animal
+    {
+        public Animal(string _name, int _noOfLegs)
+        {
+            NoOfLegs = _noOfLegs;
+            Id = Guid.NewGuid().ToString();
+            AnimalType = _name;
+        }
+        public string AnimalType { get; set; }
+        public string Id { get; set; }
+        public int NoOfLegs { get; set; }
+
+        public virtual void Run()
+        {
+            Console.WriteLine($"{AnimalType} is running");
+        }
+      
+
+        public virtual void Walk()
+        {
+            Console.WriteLine($"{AnimalType} is walking");
+        }
+    }
+}
